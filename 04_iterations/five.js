058b4a0bfd5260f30450705c128ee84f1a11e570
vaibhaviitm0925 <158093188+vaibhaviitm0925@users.noreply.github.com>
2024-01-31 09:45:50 +0000

    Iterations

---
+++ 04_iterations/five.js
@@ -0,0 +1,40 @@
+const coding = ["java", "js", "ruby", "python", "cpp"]
+
+// codding.forEach( function (val) {
+//     //console.log(val);
+// })
+
+// codding.forEach( (item) => {
+//     console.log(item);
+// })
+
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+// coding.forEach((item, index, Array) => {
+//     console.log(item,index, Array);
+// } )
+
+// const mycoding = [
+//     {
+//         languageName : "javascript",
+//         languageFileName : "js"
+//     },
+//     {
+//         languageName : "java",
+//         languageFileName : "java"
+//     },
+//     {
+//         languageName : "python",
+//         languageFileName : "phy"
+//     }
+// ]
+
+// mycoding(item >= {
+
+//     console.log(item.languageFileName);
+// })
